5c32d53656060dab02fe3d49245876ceffc9e5bd
Efraim Basilio <basilio.efraim@gmail.com>
2020-02-14 16:39:58 +0800

    update

---
+++ GOC_Tabulation_System/frmAddPicturecs.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace GOC_Tabulation_System
+{
+    partial class frmAddPicturecs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtFromDB = new System.Windows.Forms.TextBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.lrntxt = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtImage = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(605, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtFromDB
+            // 
+            this.txtFromDB.Location = new System.Drawing.Point(605, 78);
+            this.txtFromDB.Name = "txtFromDB";
+            this.txtFromDB.Size = new System.Drawing.Size(144, 20);
+            this.txtFromDB.TabIndex = 2;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(605, 149);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(144, 20);
+            this.txtPath.TabIndex = 3;
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(605, 197);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(144, 20);
+            this.txtFileName.TabIndex = 4;
+            // 
+            // lrntxt
+            // 
+            this.lrntxt.Location = new System.Drawing.Point(605, 255);
+            this.lrntxt.Name = "lrntxt";
+            this.lrntxt.Size = new System.Drawing.Size(144, 20);
+            this.lrntxt.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(494, 322);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // txtImage
+            // 
+            this.txtImage.Location = new System.Drawing.Point(605, 334);
+            this.txtImage.Name = "txtImage";
+            this.txtImage.Size = new System.Drawing.Size(144, 20);
+            this.txtImage.TabIndex = 6;
+            // 
+            // frmAddPicturecs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 634);
+            this.Controls.Add(this.txtImage);
+            this.Controls.Add(this.lrntxt);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.txtFromDB);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmAddPicturecs";
+            this.Text = "frmAddPicturecs";
+            this.Load += new System.EventHandler(this.frmAddPicturecs_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtFromDB;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.TextBox lrntxt;
+        private System.Windows.Forms.TextBox txtImage;
+    }
+}
---
+++ GOC_Tabulation_System/frmClient.cs
@@ -0,0 +1,134 @@
+﻿using GOC_Tabulation_System.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GOC_Tabulation_System
+{
+    public partial class frmClient : Form
+    {
+        public frmClient()
+        {
+            InitializeComponent();            
+        }
+
+        private void frmClient_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 18; i++)
+            {
+                dataGridView1.Rows.Add(i,"0.00");
+            }
+        }
+
+        public void AddImageDataGrid(DataGridView dgv)
+        {
+            DataGridViewImageColumn dimg = new DataGridViewImageColumn();
+            dimg.Image = Properties.Resources.close;
+            dimg.HeaderText = "Close";
+            dimg.ImageLayout = DataGridViewImageCellLayout.Normal;
+            dimg.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dgv.Columns.Add(dimg);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {            
+            int val = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            switch (val)
+            {
+                case 1:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._1;
+                    break;
+                case 2:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._2;
+                    break;
+                case 3:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._3;
+                    break;
+                case 4:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._4;
+                    break;
+                case 5:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._5;
+                    break;
+                case 6:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._6;
+                    break;
+
+                case 7:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._7;
+                    break;
+                case 8:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._8;
+                    break;
+                case 9:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._9;
+                    break;
+                case 10:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._10;
+                    break;
+                case 11:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._11;
+                    break;
+                case 12:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._12;
+                    break;
+                case 13:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._13;
+                    break;
+                case 14:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._14;
+                    break;
+                case 15:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._15;
+                    break;
+                case 16:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._16;
+                    break;
+                case 17:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._17;
+                    break;
+                case 18:
+                    pictureBox1.Image = null;
+                    pictureBox1.Image = Properties.Resources._18;
+                    break;               
+            }
+
+        }
+
+    }
+}
